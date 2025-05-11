13456bbc9bb01afa4074f675b1483e2e75ad729f
JoseeR22 <jr761715@gmail.com>
2025-05-11 13:00:11 +0200

    Completo

--- src/main/java/com/Trabajofinal/Tickets/Controllers/ReservationController.java
+++ src/main/java/com/Trabajofinal/Tickets/Controllers/ReservationController.java
@@ -13,11 +13,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.Trabajofinal.Tickets.Entities.Reservation;
 import com.Trabajofinal.Tickets.Entities.Seat;
 import com.Trabajofinal.Tickets.Services.ReservationService;
 import com.Trabajofinal.Tickets.Services.SeatService;
+
 
 @RestController
 @RequestMapping("/seats")
